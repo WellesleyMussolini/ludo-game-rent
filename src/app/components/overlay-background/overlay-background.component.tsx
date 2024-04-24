ec06ad0ec82689794b28b654dbc684e3604dd7b8
Wellesley Mussolini <iei.muss@gmail.com>
2024-04-24 13:25:16 -0300

    refactor: overlay background component for modals and forms
    - Updated overlay background to darken screen, focusing user attention on modal, form or another component that will be highlighted for the user to interact with.
    - Disabled background interaction to enhance component spotlight effect.
    - Added onclick event to allow closing the overlay by clicking outside the active component.
    
    Author: Wellesley Mussolini

--- src/app/components/overlay-background/overlay-background.component.tsx
+++ src/app/components/overlay-background/overlay-background.component.tsx
@@ -1,3 +1,10 @@
-export const OverlayBackground = ({onClick}: {onClick: () => void}) => (
-    <div className="fixed top-0 opacity-30 h-screen w-screen bg-black z-40" onClick={onClick}></div>
-);
+import React from "react";
+import { userOverlayBackground } from "./hooks/use-overlay-background.hook";
+
+export const OverlayBackground = ({ onClick }: { onClick: () => void }) => {
+    const { handleOverlayClick, useBodyLock } = userOverlayBackground(onClick);
+    useBodyLock();
+    return (
+        <div className="fixed top-0 opacity-30 h-screen w-screen bg-black z-40" onClick={handleOverlayClick}></div>
+    );
+};
