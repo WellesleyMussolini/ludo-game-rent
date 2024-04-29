59616126b7d6d6ed46da603813220790a10965cb
Wellesley Mussolini <iei.muss@gmail.com>
2024-04-29 18:03:26 -0300

    chore: deleted unnecessary files

--- src/app/(pages)/cart/components/cart-summary/cart-summary.interface.tsx
+++
@@ -1,9 +0,0 @@
-export enum EnumCartSummaryAnimation {
-    ANIMATION_SLIDE_IN = "animate-slideInFromRight",
-    ANIMATION_SLIDE_OUT = "animate-slideOutToRight"
-};
-
-export interface ICartSummary {
-    visibility: boolean,
-    handleVisibility: (visibility: boolean) => void,
-};
