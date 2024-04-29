ed86e8fcf7ee4f4b15e9c6dfc47845880aaff5b4
Wellesley Mussolini <iei.muss@gmail.com>
2024-04-29 18:28:34 -0300

    feature: function logic that removes a game from the cart array called

---
+++ src/app/(pages)/cart/utils/remove-cart-item.ts
@@ -0,0 +1,8 @@
+import { IBoardGame } from "@/interfaces/boardgame.interface";
+
+export const removeCartItem = (cart: Array<IBoardGame>, setCart: React.Dispatch<React.SetStateAction<Array<IBoardGame>>>, id: string) => {
+    if (id) {  // Ensure id is not undefined
+      const updatedCart = cart.filter((item: IBoardGame) => item.id !== id);
+      setCart(updatedCart); // Update the cart with the new array
+    };
+};
