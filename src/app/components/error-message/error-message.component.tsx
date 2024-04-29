c8d51a742476b5e0079abccb49b3031fe4f4a2e2
Wellesley Mussolini <iei.muss@gmail.com>
2024-04-29 18:04:03 -0300

    refactor: implement the error message to be responsive in smaller screens

--- src/app/components/error-message/error-message.component.tsx
+++ src/app/components/error-message/error-message.component.tsx
@@ -2,9 +2,9 @@
 
 import { IErrorMessage } from "./error-message.interface"
 
-export const ErrorMessage = ({ title, message }: IErrorMessage) => <div className="px-4 break-words flex flex-grow items-center justify-center">
-  <div className="rounded-lg bg-white p-8 text-center shadow-xl">
-    <h1 className="mb-4 text-4xl font-bold">{title}</h1>
+export const ErrorMessage = ({ title, message }: IErrorMessage) => <div className="py-8 px-4 flex flex-grow items-center justify-center">
+  <div className="rounded-lg bg-white p-8 text-center shadow-xl break-words">
+    <h1 className="mb-4 text-xl sm:text-2xl md:text-4xl font-bold">{title}</h1>
     <p className="text-gray-600">{message}</p>
   </div>
 </div>
