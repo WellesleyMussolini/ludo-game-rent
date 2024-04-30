62a4e61aac9c51fbc133a880c9849a94c31e4c3c
Wellesley Mussolini <iei.muss@gmail.com>
2024-04-30 19:40:57 -0300

    update: user profile picture size adjustsment

--- src/app/components/user-profile-picture/user-profile-picture.component.tsx
+++ src/app/components/user-profile-picture/user-profile-picture.component.tsx
@@ -4,15 +4,15 @@ import { FaCircleUser } from "react-icons/fa6";
 import { ImageComponent } from "@/app/components/image/image.component";
 import { useUserProfilePicture } from "./hooks/use-user-profile-picture.hooks";
 
-export const UserProfilePicture = () => {
+export const UserProfilePicture = ({size}: {size?: string}) => {
     const { authenticated, isLoading, session } = useUserProfilePicture();
     return (
         <>
             {isLoading || !authenticated ? (
-                <FaCircleUser className="text-gray-400 animate-pulse h-[40px] w-[40px]" />
+                <FaCircleUser className="text-gray-400 animate-pulse" size={size || 40} />
             ) : (
                 <div className="flex flex-col justify-center items-center cursor-pointer">
-                    <ImageComponent image={session?.user?.image ?? ""} alt={session?.user?.name ?? ""} className="rounded-full" width="40px" />
+                    <ImageComponent image={session?.user?.image ?? ""} alt={session?.user?.name ?? ""} className="rounded-full" width={`${size || 40}px`} />
                 </div>
             )}
         </>
