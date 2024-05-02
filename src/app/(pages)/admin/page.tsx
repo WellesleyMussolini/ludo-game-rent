a6b2cdaec6785996b31260766dd3457fb12e62c4
Wellesley Mussolini <iei.muss@gmail.com>
2024-05-02 15:20:21 -0300

    feat: private routes only allowed to be accessed by ADMINS

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -3,8 +3,18 @@
 import React from "react";
 import BoardGameCatalogue from "@/app/components/boardgame-catalogue/boardgame-catalogue.component";
 import { CreateBoardgameStepsForm } from "./components/create-boardgame-steps-form/create-boardgame-steps-form";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/utils/auth-options";
+import { getServerSession } from "next-auth";
+
+enum EnumCheckSessionRole {
+    ADMIN = "ADMIN",
+    USER = "USER",
+};
 
 export default async function Admin() {
+    const session = await getServerSession(authOptions);
+    if (session.user.role !== EnumCheckSessionRole.ADMIN) return redirect("/");
     return <div className="flex flex-col items-center justify-center min-h-screen py-10">
         <CreateBoardgameStepsForm />
         <BoardGameCatalogue />
