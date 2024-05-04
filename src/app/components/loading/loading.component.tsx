38f8a0c1e7869efff8b7aeb9cdc2917d7db1643e
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-04 17:28:45 -0300

    feat: Novo icone de loading

--- src/app/components/loading/loading.component.tsx
+++ src/app/components/loading/loading.component.tsx
@@ -1,3 +1,7 @@
 import { LoadingSpinner } from "../loading-spinner/loading-spinner.component";
 
-export const Loading = () => <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"><LoadingSpinner size={100} /></div>
+export const Loading = () => (
+  <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <LoadingSpinner size={100} />
+  </div>
+);
