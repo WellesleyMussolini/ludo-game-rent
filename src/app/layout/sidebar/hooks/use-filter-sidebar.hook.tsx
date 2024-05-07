04757031b7e7a3ca9b52e8a9420488f9bf396e88
Wellesley Mussolini <iei.muss@gmail.com>
2024-05-06 21:04:01 -0300

    features the sidebar's new settings  for the users with 'ADMIN' role
    The useFilterSidebar function has been updated to streamline the organization and display of sidebar navigation elements across our platform. This hook is designed to manage two key arrays that hold the routes, icons, and labels for the sidebar items. The first array caters to the default user experience, providing navigation options that are visible and accessible to all users. This includes standard routes with their respective icons and labels, ensuring a clear and consistent navigation experience for everyone. The second array is specifically designed for administrators, containing items that appear under the /admin route. This admin-specific navigation setup includes routes and tools necessary for administrative tasks, helping to segregate more sensitive functionalities from the general user interface. By using this structured approach, the useFilterSidebar ensures that the sidebar remains organized and tailored to user roles, facilitating a cleaner codebase and easier updates to the sidebar components as the project's needs evolve.
    
    Author: Wellesley Mussolini

--- src/app/layout/sidebar/hooks/use-filter-sidebar.hook.tsx
+++ src/app/layout/sidebar/hooks/use-filter-sidebar.hook.tsx
@@ -1,67 +1,79 @@
+"use client"
+
 import { sizeIcons } from "@/constants/size-icons";
 import { usePathname } from "next/navigation";
 import * as Fa from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
-import { MdDashboard, MdLogin } from "react-icons/md";
+import { MdLogin } from "react-icons/md";
 import { useContext } from "@/context/context";
 import { IoMdCart } from "react-icons/io";
-import { useUserSession } from "@/app/hooks/use-user-session.hook";
+import { EnumSidebarPathname } from "../interfaces/sidebar.enum";
 
 export const useFilterSidebar = () => {
     const { cart } = useContext();
-    const { isAuthenticated } = useUserSession();
     const pathName = usePathname();
     
-    let sidebarData = [
+    const sidebarDefaultData = [
         {
             icon: <Fa.FaHome size={sizeIcons.small} />,
             label: "Home",
             alert: false,
-            active: pathName === "/",
-            route: "/",
+            active: pathName === EnumSidebarPathname.HOME,
+            route: EnumSidebarPathname.HOME,
         },
         {
             icon: <FaUser size={sizeIcons.small} />,
             label: "Perfil",
             alert: false,
-            active: pathName === "/user",
-            route: "/user",
+            active: pathName === EnumSidebarPathname.USER,
+            route: EnumSidebarPathname.USER,
         },
         {
             icon: <Fa.FaInfoCircle size={sizeIcons.small} />,
             label: "Sobre",
             alert: false,
-            active: pathName === "/about",
-            route: "/about",
+            active: pathName === EnumSidebarPathname.ABOUT,
+            route: EnumSidebarPathname.ABOUT,
         },
-    ];
-
-    if (isAuthenticated) {
-        sidebarData.push({
-            icon: <MdDashboard size={sizeIcons.small} />,
-            label: "Dashboard",
-            alert: false,
-            active: pathName === "/admin",
-            route: "/admin",
-        });
-    }
-
-    sidebarData.push(
         {
             icon: <IoMdCart size={sizeIcons.small} />,
             label: "Carrinho",
             alert: cart.length !== 0,
-            active: pathName === "/cart",
-            route: "/cart",
+            active: pathName === EnumSidebarPathname.CART,
+            route: EnumSidebarPathname.CART,
         },
         {
             icon: <MdLogin size={sizeIcons.small} />,
             label: "Entrar",
             alert: false,
-            active: pathName === "/login",
-            route: "/login"
+            active: pathName === EnumSidebarPathname.AUTH,
+            route: EnumSidebarPathname.AUTH
         },
-    );
+    ];
 
-    return { sidebarData };
+    const sidebarAdminData = [
+        {
+            icon: <Fa.FaDice size={sizeIcons.small} />,
+            label: "Catálogo",
+            alert: false,
+            active: pathName === EnumSidebarPathname.ADMIN,
+            route: EnumSidebarPathname.ADMIN,
+        },
+        {
+            icon: <IoMdCart size={sizeIcons.small} />,
+            label: "Pedidos",
+            alert: false,
+            active: pathName === EnumSidebarPathname.ADMIN_ORDERS,
+            route: EnumSidebarPathname.ADMIN_ORDERS,
+        },
+        {
+            icon: <FaUser size={sizeIcons.small} />,
+            label: "Usuários",
+            alert: false,
+            active: pathName === EnumSidebarPathname.ADMIN_USERS,
+            route: EnumSidebarPathname.ADMIN_USERS,
+        },
+    ];
+
+    return { sidebarDefaultData, sidebarAdminData };
 };
