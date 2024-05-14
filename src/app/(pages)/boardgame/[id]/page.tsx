9188d0036790c6e9b37784d080b2f4cf71eb79ce
lucas rodrigues <rodrigueslucas062@gmail.com>
2024-05-13 21:35:49 -0300

    fix: Test

--- src/app/(pages)/boardgame/[id]/page.tsx
+++ src/app/(pages)/boardgame/[id]/page.tsx
@@ -3,33 +3,14 @@
 import React from "react";
 import BoardGameInfo from "./components/BoardgamesInfo/BoardgamesInfo";
 
-export default async function BoardGame({ params: { id } }: { params: { id: string } }) {
-    const boardgameData = await prisma.boardgames.findUnique({ where: { id } });
-    return (
-        <div className="flex items-center justify-center py-16 flex-col gap-4 min-h-[calc(100vh-80px)]">
-            {/* boardgame image */}
-            <ImageComponent layoutType="responsive" width="10em" image={boardgameData?.image?? ""} alt={boardgameData?.name?? ""} />
-
-            {/* boardgame name & title */}
-            <h1 className="text-3xl mt-4 font-black text-black font-cardName">{boardgameData?.name}</h1>
-
-            {/* boardgame stats */}
-            <div className="max-[350px]:px-8 flex items-center justify-center">
-                <div className="max-w-[1200px] w-full">
-                    <BoardgameStats
-                        boardgame={boardgameData}
-                        iconSize="20px"
-                        styles="flex justify-between px-10 flex-row gap-6 flex-wrap break-words w-full text-[13px] mt-4 text-gray-500"
-                    />
-                </div>
-            </div>
-
-
-            {/* rent game button */}
-            <ButtonRentGame boardgame={boardgameData} />
-            {/* <p>Descrição</p> */}
-            {/* <p className="break-words px-3">{boardgameData?.description}</p> */}
-            {/* <p>{boardgameData?.status}</p> */}
-        </div>
-    );
-};
+export default async function BoardGame({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className="flex max-md:justify-center h-screen dark:bg-zinc-800">
+      <BoardGameInfo id={id} />
+    </div>
+  );
+}
