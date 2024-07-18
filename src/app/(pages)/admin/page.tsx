ddf6f8bb0ced2d2fecea33aa04b262c5eb6b523b
Wellesley Mussolini <iei.muss@gmail.com>
2024-07-18 10:53:33 -0300

    feat: add checkAdminSession function for session validation
    - Created checkAdminSession function in the global utils folder.
    - Function checks the user session and only executes when the user tries to access any admin route.
      - If the session is null (no authentication), redirects to the admin login page.
      - If the user is authenticated but not an admin, redirects to the home page.
      - If the user is an admin, no redirection occurs.

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -3,24 +3,10 @@
 import React from "react";
 import BoardGameCatalogue from "@/app/components/boardgame-catalogue/boardgame-catalogue.component";
 import { CreateBoardgameStepsForm } from "./components/create-boardgame-steps-form/create-boardgame-steps-form";
-import { redirect } from "next/navigation";
-import { authOptions } from "@/utils/auth-options";
-import { getServerSession } from "next-auth";
-
-enum EnumCheckSessionRole {
-    ADMIN = "ADMIN",
-    USER = "USER",
-};
+import { checkAdminSession } from "@/utils/check-admin-session";
 
 export default async function Admin() {
-    const session = await getServerSession(authOptions);
-    if (session === null) {
-        return redirect("/admin/login")
-    }
-    else if (session?.user?.role !== EnumCheckSessionRole.ADMIN) {
-        return redirect("/");
-    }
-
+    await checkAdminSession();
     return <div className="flex flex-col items-center justify-center min-h-screen py-10">
         <CreateBoardgameStepsForm />
         <BoardGameCatalogue />
--- src/app/(pages)/admin/users/page.tsx
+++ src/app/(pages)/admin/users/page.tsx
@@ -1,8 +1,10 @@
 "use server"
 
+import { checkAdminSession } from "@/utils/check-admin-session";
 import { UserList } from "./components/user-list/user-list.component";
 
 export default async function Users() {
+    await checkAdminSession();
     return (
         <div className="flex justify-center items-center min-h-screen pt-24 bg-gray-50">
                 <UserList />
