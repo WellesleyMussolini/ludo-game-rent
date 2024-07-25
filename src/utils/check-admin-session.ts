45ebedeab747e62d72cdaebf28823b241e0d1a2b
Wellesley Mussolini <iei.muss@gmail.com>
2024-07-25 17:45:38 -0300

    feat: add session and role-based access control
    Implemented function logic to handle user access to admin routes. The function checks for an active session:
    - If no session is found, redirects the user to the login page.
    - If a session exists but the user lacks the admin role, redirects them to the project's home page.
    - If the user has a valid session and the admin role, allows access to the admin route.

--- src/utils/check-admin-session.ts
+++ src/utils/check-admin-session.ts
@@ -1,14 +1,14 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./auth-options";
-import { EnumCheckSessionRole } from "@/types/check-session-role.enum";
 import { EnumPathnames } from "@/types/pathnames.enum";
+import { EnumCheckUserRoles } from "@/types/check-user-role.enum";
 
 export async function checkAdminSession() {
     const session = await getServerSession(authOptions);
     if (session === null) {
         return redirect(EnumPathnames.ADMIN_LOGIN);
-    } else if (session?.user?.role !== EnumCheckSessionRole.ADMIN) {
+    } else if (session?.user?.role !== EnumCheckUserRoles.ADMIN) {
         return redirect(EnumPathnames.HOME);
     }
 };
