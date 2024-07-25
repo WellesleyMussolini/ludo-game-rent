24351256ce7eb88842f598b21de8b120ca99baa5
Wellesley Mussolini <iei.muss@gmail.com>
2024-07-25 17:41:53 -0300

    feat: add user retrieval and error handling logic
    Implemented logic to check if the Prisma indAllUsers method returns null. If so, a message box will display an error indicating that users could not be accessed. Otherwise, the user's table will display all authenticated users.

--- src/app/(pages)/admin/users/page.tsx
+++ src/app/(pages)/admin/users/page.tsx
@@ -1,13 +1,23 @@
 "use server"
 
 import { checkAdminSession } from "@/utils/check-admin-session";
-import { UserList } from "./components/user-list/user-list.component";
+import { ErrorMessage } from "@/app/components/error-message/error-message.component";
+import { UsersTable } from "./components/users-table/users-table.component";
+import { findAllUsers } from "@/utils/find-all-users";
 
 export default async function Users() {
     await checkAdminSession();
     return (
         <div className="flex justify-center items-center min-h-screen pt-24 bg-gray-50">
-                <UserList />
+            <>
+                {
+                    findAllUsers === null
+                    ?
+                    <ErrorMessage title="404" message="Não foi possível listar os usuários" />
+                    :
+                    <UsersTable />
+                }
+            </>
         </div>
     );
-}
+};
