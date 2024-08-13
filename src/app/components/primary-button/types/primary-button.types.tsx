9d7b00fd4c9ecb4b68b622ecfcefc1ebb8a6c251
Wellesley Mussolini <iei.muss@gmail.com>
2024-08-13 18:39:56 -0300

    feat: add button types and props configuration
    - Introduced an enum for primary button types, allowing style changes based on one of the 7 registered keys.
    - Defined props for the primary button component, including text name, onClick function, and disabled state, to configure button behavior and appearance.

---
+++ src/app/components/primary-button/types/primary-button.types.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+export enum EnumPrimaryButton {
+    PRIMARY="primary",
+    SECONDARY="secondary",
+    OUTLINED="outlined",
+    DISABLED="disabled",
+    GOOGLE="google",
+    DELETE="delete",
+    ALERT="alert",
+};
+
+export interface IPrimaryButton {
+    styles?: string,
+    text: string | ReactNode,
+    type: EnumPrimaryButton,
+    disabled?: boolean | undefined,
+    loadingSize?: number,
+    isLoading?: boolean,
+    onClick: (() => void), 
+};
