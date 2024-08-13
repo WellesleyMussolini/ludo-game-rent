ba78d47d48e68b4a00ceff76abbe30a4e833c9bd
Wellesley Mussolini <iei.muss@gmail.com>
2024-08-13 18:04:30 -0300

    chore: removing unnecessary files

--- src/app/components/loading/loading.component.tsx
+++
@@ -1,7 +0,0 @@
-import { LoadingSpinner } from "../loading-spinner/loading-spinner.component";
-
-export const Loading = () => (
-  <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-    <LoadingSpinner />
-  </div>
-);
