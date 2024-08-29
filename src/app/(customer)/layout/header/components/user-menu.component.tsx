a0b26770e9911f58779e329e4ee861e03f1cf0d7
Wellesley Mussolini <iei.muss@gmail.com>
2024-08-29 19:07:49 -0300

    refactor: reduce a good amount line of code with the hook file

--- src/app/(customer)/layout/header/components/user-menu.component.tsx
+++ src/app/(customer)/layout/header/components/user-menu.component.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Image from "next/image";
-import { useUserSession } from "@/app/hooks/user-session.hook";
 import { useUserMenu } from "../hooks/user-menu.hook";
 
 export const UserMenu = () => {
-    const { session } = useUserSession();
-    const { isDropdownOpen, menuOptions, handleDropdownVisibility, menuRef } = useUserMenu();
-
+    const { isDropdownOpen, menuOptions, handleDropdownVisibility, menuRef, userInfo } = useUserMenu();
     return (
         <div className="relative" ref={menuRef}>
             <Image
-                src={session?.user?.image ?? ""}
-                alt={session?.user?.name ?? ""}
+                src={userInfo.userImage}
+                alt={userInfo.userName}
                 className="cursor-pointer object-cover select-none w-12 xs:w-14 rounded-full"
                 priority={true}
                 objectFit="cover"
@@ -22,11 +19,12 @@ export const UserMenu = () => {
 
             {/* Dropdown */}
             {isDropdownOpen && (
-                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-md py-2 px-2">
+                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-md py-2 px-2 z-20">
                     {menuOptions.map((option, index) => (
                         <p
                             key={index}
                             className="block w-full text-left px-4 py-2 text-sm rounded-md text-gray-700 hover:text-white hover:bg-primary cursor-pointer"
+                            onClick={option.onClick}
                         >
                             {option.label}
                         </p>
