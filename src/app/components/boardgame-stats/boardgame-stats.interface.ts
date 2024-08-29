d7b04960430678fa9e8557fcd04fec8d79ab855c
Wellesley Mussolini <iei.muss@gmail.com>
2024-08-29 18:48:26 -0300

    chore: deleted no longer necessary file

--- src/app/components/boardgame-stats/boardgame-stats.interface.ts
+++
@@ -1,7 +0,0 @@
-import { IBoardGame } from "@/types/boardgame.interface";
-
-export interface IBoardgameStats {
-    boardgame: IBoardGame | null,
-    iconSize?: string, 
-    styles: string,
-};
--- src/app/hooks/user-session.hook.ts
+++
@@ -1,9 +0,0 @@
-import { useSession } from "next-auth/react";
-
-export const useUserSession = () => {
-    const { data: session, status } = useSession();
-    const isLoading = status === "loading";
-    const isAuthenticated = status === "authenticated";
-
-    return { session, isLoading, isAuthenticated };
-};
