5c14951b7cbf1e56d2f92989a37925e87fc7edf9
Wellesley Mussolini <iei.muss@gmail.com>
2024-08-29 19:09:23 -0300

    chore: reducing unnecessary lines of code

--- src/app/hooks/handle-rent-game.hook.ts
+++ src/app/hooks/handle-rent-game.hook.ts
@@ -1,29 +1,27 @@
-"use client"
+"use client";
 
 import { useContext } from "@/context/context";
 import { IBoardGame } from "@/types/boardgame.interface";
 import { toast } from "react-toastify";
 
 export const useHandleRentGame = (boardgame: IBoardGame | null) => {
-    const { cart, setCart } = useContext();
-  
-    const handleRentGame = async () => {
-      if (!boardgame) {
-        // Handle the case where boardgame is null
-        return; // You can choose to do nothing or log an error
-      }
-  
-      // Check if the boardgame is already in the cart
-      const isDuplicated = cart.some((item) => item.id === boardgame.id);
-  
-      // If it's not a duplicate, add it to the cart
-      if (!isDuplicated) {
-        await setCart([...cart, boardgame]);
-        toast.success(`${boardgame.name} adicionado no carrinho!`);
-      } else {
-        toast.warn(`${boardgame.name} já adicionado no carrinho.`);
-      }
+  const { cart, setCart } = useContext();
+
+  const handleRentGame = async () => {
+    // Handle the case where boardgame is null
+    if (!boardgame) return;
+
+    // Check if the boardgame is already in the cart
+    const isDuplicated = cart.some((item) => item.id === boardgame.id);
+
+    // If it's not duplicated then it'll add to the cart
+    if (!isDuplicated) {
+      setCart([...cart, boardgame]);
+      toast.success(`${boardgame.name} adicionado no carrinho!`);
+    } else {
+      toast.warn(`${boardgame.name} já adicionado no carrinho.`);
     };
-  
-    return { handleRentGame };
   };
+
+  return { handleRentGame };
+};
