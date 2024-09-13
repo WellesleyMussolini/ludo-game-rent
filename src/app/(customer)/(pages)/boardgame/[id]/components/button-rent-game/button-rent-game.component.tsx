16055f0a86ae2e9508691d646d6b368bf43e92b0
Wellesley Mussolini <iei.muss@gmail.com>
2024-09-13 00:39:36 -0300

    chore: Apply temporary changes
    - Updated the import of IBoardGame to BoardGame to simplify naming and avoid redundancy in type definitions.
    - Applied a temporary change to the import to prevent code-breaking issues, with plans for a future refactor to address the naming convention.
    - This branch is focused on authentication, so further changes will be made in a separate refactor.

--- src/app/(customer)/(pages)/boardgame/[id]/components/button-rent-game/button-rent-game.component.tsx
+++ src/app/(customer)/(pages)/boardgame/[id]/components/button-rent-game/button-rent-game.component.tsx
@@ -1,16 +1,16 @@
 "use client"
 
+import { BoardGame } from "@/app/common/types/boardgame.types";
 import { PrimaryButton } from "@/app/components/primary-button/primary-button.component"
-import { EnumPrimaryButton } from "@/app/components/primary-button/types/primary-button.types";
+import { PrimaryButtonStyles } from "@/app/components/primary-button/types/primary-button.types";
 import { useHandleRentGame } from "@/app/hooks/handle-rent-game.hook";
-import { IBoardGame } from "@/types/boardgame.interface";
 
-export const ButtonRentGame = ({ boardgame }: { boardgame: IBoardGame | null }) => {
+export const ButtonRentGame = ({ boardgame }: { boardgame: BoardGame | null }) => {
     const { handleRentGame } = useHandleRentGame(boardgame);
     if (!boardgame) return null;
     return (
         <div className="w-96 mt-4 max-[450px]:w-[80%] ring-2 ring-white rounded-md">
-            <PrimaryButton text="alugar" type={EnumPrimaryButton.PRIMARY} onClick={handleRentGame} />
+            <PrimaryButton text="alugar" type={PrimaryButtonStyles.PRIMARY} onClick={handleRentGame} />
         </div>
     );
 };
