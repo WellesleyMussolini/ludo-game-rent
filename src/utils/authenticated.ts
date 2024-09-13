4318797e4cd51c6c682fd784960020e4db07990c
Wellesley Mussolini <iei.muss@gmail.com>
2024-09-13 15:22:06 -0300

    chore: no longer necessary cookie auth logic

--- src/utils/authenticated.ts
+++
@@ -1,9 +0,0 @@
-import { AUTHENTICATION_COOKIE } from "@/app/common/constants/auth-cookie.type";
-import { cookies } from "next/headers";
-
-export const authenticated = () => {
-  const userCookie = !!cookies().get(AUTHENTICATION_COOKIE.USER)?.value;
-  const adminCookie = !!cookies().get(AUTHENTICATION_COOKIE.ADMIN)?.value;
-
-  return { userCookie, adminCookie };
-};
