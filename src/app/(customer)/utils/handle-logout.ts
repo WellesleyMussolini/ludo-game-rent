b47f61bd744b50295a06e5a8ded33d5de1042684
Wellesley Mussolini <iei.muss@gmail.com>
2024-09-13 00:31:07 -0300

    chore: update authentication cookie import path

--- src/app/(customer)/utils/handle-logout.ts
+++ src/app/(customer)/utils/handle-logout.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { AUTHENTICATION_COOKIE } from "@/app/(auth)/constants/auth-cookie.type";
+import { AUTHENTICATION_COOKIE } from "@/app/common/constants/auth-cookie.type";
 import { cookies } from "next/headers";
 
 export const handleLogout = async () => {
