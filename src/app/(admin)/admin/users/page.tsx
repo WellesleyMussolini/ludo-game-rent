03a6bbe9465505c734a6c0cc0d9e6292d4816a95
Wellesley Mussolini <iei.muss@gmail.com>
2024-09-30 12:52:43 -0300

    refactor: remove redundant null check for findAllUsers; the length check is sufficient to determine if the database is not empty

--- src/app/(admin)/admin/users/page.tsx
+++ src/app/(admin)/admin/users/page.tsx
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { ErrorMessage } from "@/app/components/error-message/error-message.component";
 import { UsersTable } from "./components/users-table/users-table.component";
@@ -6,15 +6,16 @@ import { findAllUsers } from "@/services/find-all-users.service";
 import { IUser } from "@/app/common/types/user.interface";
 
 export default async function Users() {
-    return (
-        <div className="flex justify-center items-center">
-            {
-                findAllUsers === null || findAllUsers.length === 0
-                    ?
-                    <ErrorMessage title="404" message="Não foi possível listar os usuários" />
-                    :
-                    <UsersTable allUsers={findAllUsers as Array<IUser>} />
-            }
-        </div>
-    );
-};
+  return (
+    <div className="flex justify-center items-center">
+      {findAllUsers.length === 0 ? (
+        <ErrorMessage
+          title="404"
+          message="Não foi possível listar os usuários"
+        />
+      ) : (
+        <UsersTable allUsers={findAllUsers as Array<IUser>} />
+      )}
+    </div>
+  );
+}
