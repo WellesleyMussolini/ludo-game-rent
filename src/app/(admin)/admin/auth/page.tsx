dbb2de0caf6a64e663b20d6317a3bdf74318f4b2
Wellesley Mussolini <iei.muss@gmail.com>
2024-10-03 00:05:46 -0300

    refactor: remove outdated session check logic from admin auth page
    - Removed unnecessary logic that was checking the user's session and redirecting based on it.
    - Middleware now handles authentication and redirection, eliminating the need for page-specific logic.

--- src/app/(admin)/admin/auth/page.tsx
+++ src/app/(admin)/admin/auth/page.tsx
@@ -1,13 +1,12 @@
-"use server"
+"use server";
 
 import { EnumAuth } from "@/app/components/auth/types/auth.types";
-import { authOptions } from "../../../(auth)/utils/auth-options";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { Auth as AuthComponent } from "../../../components/auth/auth.component";
 
 export default async function Auth() {
-    const session = await getServerSession(authOptions);
-    if (session !== null) return redirect("/admin");
-    return <div className="flex items-center justify-center w-full min-h-screen pt-24"><AuthComponent type={EnumAuth.ADMIN} /></div>
-};
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <AuthComponent type={EnumAuth.ADMIN} />
+    </div>
+  );
+}
