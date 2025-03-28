52626b19da74ba148509a79d95c87ee939fd1d07
Wellesley Mussolini <iei.muss@gmail.com>
2025-03-27 21:53:47 -0300

    deleted nolonger necessary files

--- src/app/common/components/card/user-profile/hooks/use-user-profile-picture.hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useSession } from "next-auth/react";
-
-export const useUserProfilePicture = () => {
-  const { status } = useSession();
-  const isLoading = status === "loading";
-  const authenticated = status === "authenticated";
-  return { isLoading, authenticated };
-};
